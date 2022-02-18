4b46caf3302a68a3f20c1df6a60630059cc81127
Ben Hanowell <ben@hanowell.me>
2022-02-17 20:21:47 -0800

    Add observed death counts

--- R/data-collection.R
+++ R/data-collection.R
@@ -5,8 +5,8 @@ library(dplyr)
 library(readr)
 library(stringr)
 
-# Age-specific death rates for Russia in 1997 ----
-rus_mxi <- readr::read_csv("raw/RUS_m_short_idr.csv") %>%
+# Age-specific death rates and death counts for Russia in 1997 ----
+rus_Mxi <- readr::read_csv("raw/RUS_m_short_idr.csv") %>%
   dplyr::filter(sex == 3, year == 1997) %>%
   dplyr::mutate(disease_type = dplyr::case_when(
     cause == 0 ~ "all",
@@ -20,22 +20,49 @@ rus_mxi <- readr::read_csv("raw/RUS_m_short_idr.csv") %>%
     cols = m0:m85p,
     names_prefix = "m",
     names_to = "x",
-    values_to = "mx"
+    values_to = "Mx"
   ) %>%
   tidyr::pivot_wider(
     id_cols = x,
     names_from = disease_type,
-    values_from = mx,
-    names_prefix = "mx_"
+    values_from = Mx,
+    names_prefix = "Mx_"
   ) %>%
   dplyr::mutate(
     x = as.numeric(stringr::str_replace_all(x, "p", ""))
   ) %>%
-  dplyr::select(-mx_all)
-saveRDS(rus_mxi, "data/rus_mxi.rds")
+  dplyr::select(-Mx_all)
+rus_Dxi <- readr::read_csv("raw/RUS_d_short_idr.csv") %>%
+  dplyr::filter(sex == 3, year == 1997) %>%
+  dplyr::mutate(disease_type = dplyr::case_when(
+    cause == 0 ~ "all",
+    cause == 7 ~ "heart",
+    cause %in% c(1:6, 8:16) ~ "other"
+  )) %>%
+  dplyr::group_by(disease_type) %>%
+  dplyr::summarize_at(vars(d0:d85p), sum) %>%
+  dplyr::ungroup() %>%
+  tidyr::pivot_longer(
+    cols = d0:d85p,
+    names_prefix = "d",
+    names_to = "x",
+    values_to = "dx"
+  ) %>%
+  tidyr::pivot_wider(
+    id_cols = x,
+    names_from = disease_type,
+    values_from = dx,
+    names_prefix = "Dx_"
+  ) %>%
+  dplyr::mutate(
+    x = as.numeric(stringr::str_replace_all(x, "p", ""))
+  ) %>%
+  dplyr::select(-Dx_all)
+rus_Mxi_Dxi <- rus_Mxi %>% dplyr::left_join(rus_Dxi)
+saveRDS(rus_Mxi_Dxi, "data/rus_Mxi_Dxi.rds")
 
-# Age-specific death rates for Japan in 1997 ----
-jpn_mxi <- readr::read_csv("raw/JPN_m_short_idr.csv") %>%
+# Age-specific death rates and death counts for Japan in 1997 ----
+jpn_Mxi <- readr::read_csv("raw/jpn_m_short_idr.csv") %>%
   dplyr::filter(sex == 3, year == 1997) %>%
   dplyr::mutate(disease_type = dplyr::case_when(
     cause == 0 ~ "all",
@@ -49,16 +76,43 @@ jpn_mxi <- readr::read_csv("raw/JPN_m_short_idr.csv") %>%
     cols = m0:m85p,
     names_prefix = "m",
     names_to = "x",
-    values_to = "mx"
+    values_to = "Mx"
   ) %>%
   tidyr::pivot_wider(
     id_cols = x,
     names_from = disease_type,
-    values_from = mx,
-    names_prefix = "mx_"
+    values_from = Mx,
+    names_prefix = "Mx_"
   ) %>%
   dplyr::mutate(
     x = as.numeric(stringr::str_replace_all(x, "p", ""))
   ) %>%
-  dplyr::select(-mx_all)
-saveRDS(jpn_mxi, "data/jpn_mxi.rds")
+  dplyr::select(-Mx_all)
+jpn_Dxi <- readr::read_csv("raw/jpn_d_short_idr.csv") %>%
+  dplyr::filter(sex == 3, year == 1997) %>%
+  dplyr::mutate(disease_type = dplyr::case_when(
+    cause == 0 ~ "all",
+    cause == 7 ~ "heart",
+    cause %in% c(1:6, 8:16) ~ "other"
+  )) %>%
+  dplyr::group_by(disease_type) %>%
+  dplyr::summarize_at(vars(d0:d85p), sum) %>%
+  dplyr::ungroup() %>%
+  tidyr::pivot_longer(
+    cols = d0:d85p,
+    names_prefix = "d",
+    names_to = "x",
+    values_to = "dx"
+  ) %>%
+  tidyr::pivot_wider(
+    id_cols = x,
+    names_from = disease_type,
+    values_from = dx,
+    names_prefix = "Dx_"
+  ) %>%
+  dplyr::mutate(
+    x = as.numeric(stringr::str_replace_all(x, "p", ""))
+  ) %>%
+  dplyr::select(-Dx_all)
+jpn_Mxi_Dxi <- jpn_Mxi %>% dplyr::left_join(jpn_Dxi)
+saveRDS(jpn_Mxi_Dxi, "data/jpn_Mxi_Dxi.rds")
